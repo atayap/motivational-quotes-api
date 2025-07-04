6429e05859a70d4cc952dbb3c03f3d2e765ddd26
Atya-web678 <atayafikri624@gmail.com>
2025-07-04 15:29:07 +0700

    quotes

--- api/quotes.js
+++ api/quotes.js
@@ -1,10 +1,22 @@
 const quotes = require('../quotes.json');
 
 module.exports = (req, res) => {
+  // Tambahkan header CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Tangani preflight request (OPTIONS)
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  // Logic utama
   const { query } = req;
   if (query.random === 'true') {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     return res.status(200).json(quotes[randomIndex]);
   }
+
   res.status(200).json(quotes);
 };
